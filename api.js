f2972da5d2ad5a5550b33d659eaee3fe5082a688
Theuz987 <mathamorim52@gmail.com>
2024-10-09 21:45:42 -0300

    objeto comentado para rodar o meu no api.js

--- api.js
+++ api.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 app.use( express.json() )
 
-const cr_nota = require('./routes/nota')
-app.use('/nota', cr_nota)
+//const cr_nota = require('./routes/nota')
+//app.use('/nota', cr_nota)
 
-const cr_user = require('./routes/user')
-app.use('/user', cr_user)
+//const cr_user = require('./user')
+//app.use('/user', cr_user)
 
 const cr_music = require('./routes/musica')
 app.use('/Musica', cr_music)
