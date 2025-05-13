7ffc2c6701b43fa21387624a1c34264d4e067294
Theuz987 <mathamorim52@gmail.com>
2025-05-12 21:48:27 -0300

    ubdsf

--- index.js
+++ index.js
@@ -199,3 +199,10 @@ app.listen(port, () => {
     console.log(`REST API iniciada. Servidor está em http://localhost:${port})`);
     });
   
+//     # Environment variables declared in this file are automatically made available to Prisma.
+// # See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
+
+// # Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
+// # See the documentation for all the connection string options: https://pris.ly/d/connection-strings
+
+// DATABASE_URL="mysql://root:@10.60.44.70:3306/sonus"
