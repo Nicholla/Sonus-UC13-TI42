a8caca356560944bfef6c167ead6b7e4f98ae4c7
Theuz987 <mathamorim52@gmail.com>
2025-05-13 22:14:10 -0300

    correção e alterações no schema.prisma e index.js

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ app.use('/user', cr_user)*/
 
 
 
-app.get("/albuns", async (req, res) => {
-    const albuns = await prisma.album.findMany();
-    res.json(albuns);
+app.get("/album", async (req, res) => {
+    const album = await prisma.album.findMany();
+    res.json(album);
 });
 
-app.get("/albuns/:id", async (req, res) => {
+app.get("/album/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     const album = await prisma.album.findUnique( {where:{id}} );
     if (album === null){
@@ -35,29 +35,13 @@ app.get("/albuns/:id", async (req, res) => {
     }
 });
 
-// app.get("/albuns/recentes", async (req, res) => {
-//     try {
-//         const albuns = await prisma.album.findMany({
-//             orderBy: {
-//                 id: 'desc' // ou 'createdAt', se disponível
-//             },
-//             take: 5
-//         });
-//         res.json(albuns);
-//     } catch (error) {
-//         res.status(500).send("Erro ao buscar álbuns recentes.");
-//     }
-// });
 
-
-
-
-app.get("/artistas", async (req, res) => {
-    const artistas = await prisma.artista.findMany();
-    res.json(artistas);
+app.get("/artista", async (req, res) => {
+    const artista = await prisma.artista.findMany();
+    res.json(artista);
 });
 
-app.get("/artistas/:id", async (req, res) => {
+app.get("/artista/:id", async (req, res) => {
     const id = parseInt(req.params.id);
     const artista = await prisma.artista.findUnique( {where:{id}} );
     if (artista === null){
@@ -66,24 +50,6 @@ app.get("/artistas/:id", async (req, res) => {
         res.json(artista);
     }
 });
-
-
-
-app.get("/avaliacao", async (req, res) => {
-    const avaliacao = await prisma.avaliacao.findMany();
-    res.json(avaliacao);
-});
-
-app.get("/avaliacao/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const artista = await prisma.avaliacao.findUnique( {where:{id}} );
-    if (artista === null){
-        res.status(404).send("Avaliação não encontrada");
-    }else{
-        res.json(avaliacao);
-    }
-});
-
 
 
 app.get("/musica", async (req, res) => {
@@ -101,39 +67,23 @@ app.get("/musica/:id", async (req, res) => {
     }
 });
 
-// app.get("/musica/recentes", async (req, res) => {
-//     try {
-//         const musicas = await prisma.musica.findMany({
-//             orderBy: {
-//                 id: 'desc' // ou 'createdAt', se tiver um campo de data
-//             },
-//             take: 15
-//         });
-//         res.json(musicas);
-//     } catch (error) {
-//         res.status(500).send("Erro ao buscar músicas recentes adicionadas.");
-//     }
-// });
-
-
-app.get("/usuarios", async (req, res) => {
-    const usuarios = await prisma.usuarios.findMany();
-    res.json(usuarios);
+app.get("/usuario", async (req, res) => {
+    const usuario = await prisma.usuario.findMany();
+    res.json(usuario);
 });
 
-app.get("/usuarios/:id", async (req, res) => {
+app.get("/usuario/:id", async (req, res) => {
     const id = parseInt(req.params.id);
-    const usuarios = await prisma.usuarios.findUnique( {where:{id}} );
-    if (usuarios === null){
+    const usuario = await prisma.usuario.findUnique( {where:{id}} );
+    if (usuario === null){
         res.status(404).send("Usuário não encontrado");
     }else{
-        res.json(usuarios);
+        res.json(usuario);
     }
 });
 
 
-
-app.post("/usuarios", async (req, res) => {
+app.post("/usuario", async (req, res) => {
     if ((req.body.nome === undefined) || (req.body.preco === undefined)){
         res.status(400).send("Campos obrigatórios faltantes");
     }else{
@@ -141,9 +91,25 @@ app.post("/usuarios", async (req, res) => {
             nome:req.body.nome,
             preco: req.body.preco
         }});
-        res.status(201).location(`/usuarios/${novoUsuario.id}`).send();
+        res.status(201).location(`/usuario/${novoUsuario.id}`).send();
     }
-})
+});
+
+
+app.get("/preferencias", async (req, res) => {
+    const preferencias = await prisma.preferencias.findMany();
+    res.json(preferencias);
+});
+
+app.get("/preferencias/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const preferencias = await prisma.preferencias.findUnique( {where:{id}} );
+    if (preferencias === null){
+        res.status(404).send("Preferência não encontrada");
+    }else{
+        res.json(preferencias);
+    }
+});
 
 
 // app.post("/usuarios", async (req, res) => {
@@ -158,49 +124,40 @@ app.post("/usuarios", async (req, res) => {
 //     }
 // })
 
-app.put("/produtos/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    if ((req.body.nome === undefined) || (req.body.preco === undefined)){
-        res.status(400).send("Campos obrigatórios faltantes");
-    }else{
-        try{
-        await prisma.produto.update({
-            where: { id },
-            data: {
-                nome: req.body.nome,
-                preco: req.body.preco
-                }  
-            });
-            res.status(204).send();
-        } catch(error){
-            res.status(404).send("Produto não encontrado");
-        }
-    }
-})
+// app.put("/produtos/:id", async (req, res) => {
+//     const id = parseInt(req.params.id);
+//     if ((req.body.nome === undefined) || (req.body.preco === undefined)){
+//         res.status(400).send("Campos obrigatórios faltantes");
+//     }else{
+//         try{
+//         await prisma.produto.update({
+//             where: { id },
+//             data: {
+//                 nome: req.body.nome,
+//                 preco: req.body.preco
+//                 }  
+//             });
+//             res.status(204).send();
+//         } catch(error){
+//             res.status(404).send("Produto não encontrado");
+//         }
+//     }
+// })
 
 
-
-app.delete("/produtos/:id", async (req, res)=> {
-    const id = parseInt(req.params.id);
-    try{
-    await prisma.produto.delete({
-        where: { id }
-    });
-    res.status(202).send();
-    } catch(error){
-        res.status(404).send("Produto não encontrado");
-    }
-});
-
+// app.delete("/produtos/:id", async (req, res)=> {
+//     const id = parseInt(req.params.id);
+//     try{
+//     await prisma.produto.delete({
+//         where: { id }
+//     });
+//     res.status(202).send();
+//     } catch(error){
+//         res.status(404).send("Produto não encontrado");
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`REST API iniciada. Servidor está em http://localhost:${port})`);
     });
   
-//     # Environment variables declared in this file are automatically made available to Prisma.
-// # See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
-
-// # Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
-// # See the documentation for all the connection string options: https://pris.ly/d/connection-strings
-
-// DATABASE_URL="mysql://root:@10.60.44.70:3306/sonus"
